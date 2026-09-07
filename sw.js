7999e79a1043df74319014a0b6ca7b2aa81700d6
louisjung24-max <louisjung24@gmail.com>
2026-09-08 00:48:03 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // 항상 서버에서 먼저 받고, 오프라인일 때만 저장본을 씁니다.
-const K='salgm-v11';
+const K='salgm-v12';
 self.addEventListener('install',e=>{self.skipWaiting();});
 self.addEventListener('activate',e=>e.waitUntil(
   caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==K).map(k=>caches.delete(k)))).then(()=>self.clients.claim())
