c37d320b3a4c12f00635e2155ba714f4f4188d77
louisjung24-max <louisjung24@gmail.com>
2026-09-07 16:46:31 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-const K='salgm-v1', F=['./','./index.html','./manifest.json','./icon.svg'];
+const K='salgm-v2', F=['./','./index.html','./manifest.json','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(K).then(c=>c.addAll(F))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
